035a1df874a7fe884e8f46ce5afc12f5aa65ef60
Marcelo <mrcelo@gmail.com>
2024-01-10 21:16:29 -0300

    add group

--- Project-schedule/app/public/js/scriptHome/index.js
+++ Project-schedule/app/public/js/scriptHome/index.js
@@ -2,19 +2,15 @@ export function toggleMenu() {
   console.log("vwe");
   var containerAsideMenu = document.querySelector(".container-side-bar");
   var sideBar = document.querySelector(".side-bar-left");
-  const containerContent = document.querySelector(".container-aside-menu");
+  const containerContent = document.querySelector(".container-content");
 
-  if (containerAsideMenu.style.width === "250px") {
+  if (containerAsideMenu.style.width === "200px") {
     containerAsideMenu.style.width = "0";
-    containerAsideMenu.style.height = "0";
-    // conteudo.style.marginLeft = "0";
+    // containerAsideMenu.style.height = "0";
+    containerAsideMenu.style.maxWidth = ""; // Removendo a maxWidth quando a largura for igual a "200px"
   } else {
-    // window.getComputedStyle().width
-
-    containerAsideMenu.style.width = "250px";
-    containerAsideMenu.style.height = "100%";
-    // conteudo.style.marginLeft = "250px";
-
-    // containerContent.style.width = "";
+    containerAsideMenu.style.width = "200px";
+    // containerAsideMenu.style.height = "100%";
+    containerAsideMenu.style.maxWidth = "200px"; // Adicionando a maxWidth quando a largura for diferente de "200px"
   }
 }
